c1187e08b244af202f66342cdd69a95cca7576a3
Wiktor Wilński <wilinskiwiktor27@gmail.com>
2026-04-25 15:31:41 +0200

    Create BaseEntity and Event entity for database

---
+++ src/main/java/com/portfolio/ebilet/reservations/domain/BaseEntity.java
@@ -0,0 +1,43 @@
+package com.portfolio.ebilet.reservations.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.Version;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@NoArgsConstructor
+@SuperBuilder
+@Getter
+@Setter
+public abstract class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Version
+    @Column(nullable = false)
+    private Long version;
+
+    @CreatedDate
+    @Column(updatable = false, nullable = false)
+    private Instant createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private Instant updatedAt;
+}
---
+++ src/main/java/com/portfolio/ebilet/reservations/domain/Event.java
@@ -0,0 +1,49 @@
+package com.portfolio.ebilet.reservations.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+
+@Entity
+@Table(name = "events")
+@NoArgsConstructor
+@SuperBuilder
+@Getter(AccessLevel.PACKAGE)
+@Setter(AccessLevel.PACKAGE)
+class Event extends BaseEntity{
+
+    @Column(nullable = false)
+    @NotNull
+    private String name;
+
+    private String description;
+
+    @Column(nullable = false)
+    @NotNull
+    private Instant startDate;
+
+    @Column(nullable = false)
+    @NotNull
+    private Instant endDate;
+
+    @Column(nullable = false)
+    @NotNull
+    private String city;
+
+    @Column(nullable = false)
+    @NotNull
+    private String location;
+
+    @Column(nullable = false)
+    @NotNull
+    private BigDecimal price;
+}
