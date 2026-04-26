d1c8cc93cc65bfc4bbe7a2946fc6a7214c48e13e
Wiktor Wilński <wilinskiwiktor27@gmail.com>
2026-04-26 12:48:00 +0200

    Add repositories to created entities

---
+++ src/main/java/com/portfolio/ebilet/reservations/domain/SeatRepository.java
@@ -0,0 +1,8 @@
+package com.portfolio.ebilet.reservations.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SeatRepository extends JpaRepository<Seat, String> {
+}
