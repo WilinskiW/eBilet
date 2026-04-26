8050ff55bfda2309b10e48dd589d591217b71b27
Wiktor Wilński <wilinskiwiktor27@gmail.com>
2026-04-26 13:02:32 +0200

    Prepare classes to integration tests

---
+++ src/test/java/com/portfolio/ebilet/domain/SeatIntegrationTest.java
@@ -0,0 +1,12 @@
+package com.portfolio.ebilet.domain;
+
+import com.portfolio.ebilet.BaseIntegrationTest;
+import org.junit.jupiter.api.Test;
+
+public class SeatIntegrationTest extends BaseIntegrationTest {
+
+    @Test
+    void shouldThrowOptimisticLockingExceptionWhenConcurrentUpdateOccurs(){
+
+    }
+}
